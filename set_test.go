ba60ffdbbc78c9640a4dab62bb678617a750396e
Phil Hanna <ph1204@gmail.com>
2023-07-18 04:11:05 -0400

    Added unit tests for Delete and Len

--- set_test.go
+++ set_test.go
@@ -11,15 +11,51 @@ func TestAdd(t *testing.T) {
 	this.Add(3)
 	this.Add(1)
 	this.Add(3)
-	assert.Equal(t, 2, len(this.list))
+	assert.Equal(t, 2, this.Len())
 }
 
 func TestContains(t *testing.T) {
 	list := []string{"Larry", "Curly", "Moe", "Curly"}
-	set := NewSet[string](list...)
-	assert.True(t, set.Contains("Larry"))
-	assert.True(t, set.Contains("Curly"))
-	assert.False(t, set.Contains("Curly Joe"))
+	this := NewSet[string](list...)
+	assert.True(t, this.Contains("Larry"))
+	assert.True(t, this.Contains("Curly"))
+	assert.False(t, this.Contains("Curly Joe"))
+}
+
+func TestDelete(t *testing.T) {
+
+	getOrderedSet := func() Set[string] {
+		orderedSet := NewSet[string]()
+		orderedSet.Add("A")
+		orderedSet.Add("B")
+		orderedSet.Add("C")
+		orderedSet.Add("D")
+		return orderedSet
+	}
+	assert.Equal(t, []string{"A", "B", "C", "D"}, getOrderedSet().list)
+
+	tests := []struct {
+		name   string
+		set    Set[string]
+		item   string
+		newSet Set[string]
+	}{
+		{"Delete Curly", NewSet[string]("Larry", "Curly", "Moe"), "Curly", NewSet[string]("Moe", "Larry")},
+		{"Delete Curly from singleton", NewSet[string]("Curly"), "Curly", NewSet[string]()},
+		{"Delete from empty set", NewSet[string](), "Bogus", NewSet[string]()},
+		{"Delete first", getOrderedSet(), "A", NewSet[string]("B", "C", "D")},
+		{"Delete middle", getOrderedSet(), "C", NewSet[string]("A", "B", "D")},
+		{"Delete last", getOrderedSet(), "D", NewSet[string]("A", "B", "C")},
+		{"Delete not contains", getOrderedSet(), "E", NewSet[string]("A", "B", "C", "D")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := tt.set
+			that := tt.newSet
+			this.Delete(tt.item)
+			assert.True(t, this.Equal(that))
+		})
+	}
 }
 
 func TestIsSubset(t *testing.T) {
