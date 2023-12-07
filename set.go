fab6c7a19215bb1834e5fd281947ffb798e1b653
Phil Hanna <ph1204@gmail.com>
2023-12-07 17:03:44 -0500

    Changed type from comparable to cmp.Ordered
    Fix for issue #6

--- set.go
+++ set.go
@@ -9,12 +9,13 @@ import "cmp"
 
 // Set is an unordered list of type T. Elements of type T must be Ordered
 // according to the definition in https://pkg.go.dev/cmp#Ordered:
-// type Ordered interface {
-//	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-//		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
-//		~float32 | ~float64 |
-//		~string
-// }
+//
+//	type Ordered interface {
+//		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+//			~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+//			~float32 | ~float64 |
+//			~string
+//	}
 type Set[T cmp.Ordered] struct {
 	list []T
 }
